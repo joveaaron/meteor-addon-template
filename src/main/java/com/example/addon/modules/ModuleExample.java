2a58fd36c4e1d10c49729091e3dd434acc0d7f9b
Aarón García Egea <79792352+joveaaron@users.noreply.github.com>
2025-03-15 00:42:12 +0100

    Update ModuleExample.java

--- src/main/java/com/example/addon/modules/ModuleExample.java
+++ src/main/java/com/example/addon/modules/ModuleExample.java
@@ -1,66 +1,88 @@
 package com.example.addon.modules;
 
-import com.example.addon.AddonTemplate;
-import meteordevelopment.meteorclient.events.render.Render3DEvent;
-import meteordevelopment.meteorclient.renderer.ShapeMode;
-import meteordevelopment.meteorclient.settings.ColorSetting;
-import meteordevelopment.meteorclient.settings.DoubleSetting;
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.events.world.ChunkDataEvent;
+import meteordevelopment.meteorclient.settings.IntSetting;
 import meteordevelopment.meteorclient.settings.Setting;
 import meteordevelopment.meteorclient.settings.SettingGroup;
+import meteordevelopment.meteorclient.settings.StringSetting;
+import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Module;
-import meteordevelopment.meteorclient.utils.render.color.Color;
-import meteordevelopment.meteorclient.utils.render.color.SettingColor;
+import meteordevelopment.meteorclient.utils.player.ChatUtils;
 import meteordevelopment.orbit.EventHandler;
+import net.minecraft.block.Blocks;
+import net.minecraft.text.Text;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Box;
+import net.minecraft.world.chunk.Chunk;
 
-public class ModuleExample extends Module {
-    private final SettingGroup sgGeneral = this.settings.getDefaultGroup();
-    private final SettingGroup sgRender = this.settings.createGroup("Render");
+import java.io.*;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-    /**
-     * Example setting.
-     * The {@code name} parameter should be in kebab-case.
-     * If you want to access the setting from another class, simply make the setting {@code public}, and use
-     * {@link meteordevelopment.meteorclient.systems.modules.Modules#get(Class)} to access the {@link Module} object.
-     */
-    private final Setting<Double> scale = sgGeneral.add(new DoubleSetting.Builder()
-        .name("scale")
-        .description("The size of the marker.")
-        .defaultValue(2.0d)
-        .range(0.5d, 10.0d)
-        .build()
-    );
+public class BedrockFinder extends Module {
+    SettingGroup sgDefault = settings.getDefaultGroup();
+    private final Setting<Integer> searchY = sgDefault.add(new IntSetting.Builder()
+            .name("Y")
+            .range(-64, 128)
+            .sliderRange(-64, 128)
+                    .defaultValue(123)
+            .build());
+    private final Setting<String> savePath = sgDefault.add(new StringSetting.Builder()
+            .name("Path")
+            .defaultValue("D:/br.txt")
+            .build());
+    public static ExecutorService executor = Executors.newSingleThreadExecutor();
 
-    private final Setting<SettingColor> color = sgRender.add(new ColorSetting.Builder()
-        .name("color")
-        .description("The color of the marker.")
-        .defaultValue(Color.MAGENTA)
-        .build()
-    );
+    public BedrockFinder() {
+        super(Addon.CATEGORY, "BedrockFinder", "");
+    }
+   LinkedHashSet<BlockPos> bedrockPos = new LinkedHashSet<>();
 
-    /**
-     * The {@code name} parameter should be in kebab-case.
-     */
-    public ModuleExample() {
-        super(AddonTemplate.CATEGORY, "world-origin", "An example module that highlights the center of the world.");
+    @EventHandler
+    private void onChunkData(ChunkDataEvent event) {
+        executor.execute(()->{
+            Chunk c = event.chunk();
+            for (int x = c.getPos().getStartX(); x <= c.getPos().getEndX(); x++) {
+                for (int z = c.getPos().getStartZ(); z <= c.getPos().getEndZ(); z++) {
+                    BlockPos sPos = new BlockPos(x, searchY.get(), z);
+                    if (c.getBlockState(sPos).getBlock().equals(Blocks.BEDROCK))
+                        bedrockPos.add(sPos);
+                }
+            }
+        });
     }
 
-    /**
-     * Example event handling method.
-     * Requires {@link AddonTemplate#getPackage()} to be setup correctly, otherwise the game will crash whenever the module is enabled.
-     */
-    @EventHandler
-    private void onRender3d(Render3DEvent event) {
-        // Create & stretch the marker object
-        Box marker = new Box(BlockPos.ORIGIN);
-        marker = marker.stretch(
-            scale.get() * marker.getLengthX(),
-            scale.get() * marker.getLengthY(),
-            scale.get() * marker.getLengthZ()
-        );
-
-        // Render the marker based on the color setting
-        event.renderer.box(marker, color.get(), color.get(), ShapeMode.Both, 0);
+    @Override
+    public void onDeactivate() {
+        BufferedWriter bw = null;
+        try {
+            File f = new File(savePath.get());
+            if (f.exists() && f.canWrite()) {
+                bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(f)));
+                for (BlockPos pos : bedrockPos) {
+                    String s = pos.getX() + " " + pos.getY() + " " + pos.getZ() +
+                            " Bedrock";
+                    bw.write(s);
+                    bw.newLine();
+                }
+                bw.flush();
+                bw.close();
+            }else {
+                ChatUtils.sendMsg(Text.of("File Not Found"));
+            }
+        } catch (Exception e) {
+            ChatUtils.sendMsg(Text.of(e.getMessage()));
+        } finally {
+            bedrockPos.clear();
+            try {
+                if (bw != null) {
+                    bw.close();
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
